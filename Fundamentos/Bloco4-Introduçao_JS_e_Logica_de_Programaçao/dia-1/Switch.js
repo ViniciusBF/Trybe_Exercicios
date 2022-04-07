554b51a150f731979216866904b0262b69d63044
Vinícius <viniciusgolfera@gmail.com>
2022-04-07 09:41:56 -0300

    finalizando switch case

---
+++ Fundamentos/Bloco4-Introduçao_JS_e_Logica_de_Programaçao/dia-1/Switch.js
@@ -0,0 +1,19 @@
+let estadoDoCandidato = "ApRoVaDa"
+
+switch (estadoDoCandidato.toLowerCase()) {
+  case "aprovada" :
+    console.log("aprovada");
+    break;
+
+  case "lista" :
+    console.log("lista");
+    break;
+
+  case "reprovada" :
+    console.log("reprovada");
+    break;
+
+  default:
+    console.log("não se aplica");
+    break;
+}
