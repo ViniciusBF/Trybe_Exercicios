81bb77286b30a3bec806486238110a8d456d12f5
Vinícius <viniciusgolfera@gmail.com>
2022-04-11 10:35:57 -0300

    finalizando exercicios

---
+++ Fundamentos/Bloco4-Introduçao_JS_e_Logica_de_Programaçao/dia-3/exercicios.js
@@ -0,0 +1,56 @@
+// let fatorial = 10;
+// let resultado = fatorial;
+
+// for(let index = fatorial - 1; index > 0; index -= 1) {
+//   resultado *= index;
+// }
+
+// console.log(resultado);
+
+// // // // // // // // FIM DO EXERCICIO 1
+
+// let word = 'Funciona';
+// let reverter = '';
+
+// for(let index = word.length - 1; index >= 0; index -= 1) {
+//   reverter += word[index]
+// }
+// console.log(reverter);
+
+// // // // // // // // FIM DO EXERCICIO 2
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let maiorPalavra = array[0];
+// let menorPalavra = array[0];
+
+// for(let index = 0; index < array.length; index += 1) {
+//   if(array[index].length > maiorPalavra.length) {
+//     maiorPalavra = array[index]
+//   } else if(array[index].length < menorPalavra.length) {
+//     menorPalavra = array[index]
+//   }
+// }
+
+// console.log(menorPalavra, maiorPalavra)
+
+// // // // // // // // FIM DO EXERCICIO 3
+
+// let numerosEntre = 50
+// let primos = []
+
+
+// for(let index = 1; index <= numerosEntre; index += 1) {
+//   let valores = 0;
+//   for(let i2 = 0; i2 < index; i2 += 1) {
+//     if(index % i2 === 0) {
+//       valores += 1;
+//     }
+//   }
+//   if(valores === 1) {
+//     primos.push(index)
+//   }
+// }
+// // console.log(primos)
+// console.log(primos[primos.length - 1])
+
+// // // // // // // // FIM DO EXERCICIO 4
