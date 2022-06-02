2b9efcbb257a1ade8a78b5f0b309695c93ada21a
Vinícius <viniciusgolfera@gmail.com>
2022-06-02 00:41:09 -0300

    finalizando exercicio bonus

--- Front_End/Bloco9-JavaScript_e_Testes_Assíncronos/dia-2/exercicio/exercicio2.js
+++ Front_End/Bloco9-JavaScript_e_Testes_Assíncronos/dia-2/exercicio/exercicio2.js
@@ -1,20 +1,39 @@
-const url = 'https://api.coincap.io/v2/assets';
+const fetchCripto = async () => {
+  const cUrl = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/brl.min.json`;
+
+  const final = await fetch(cUrl)
+    .then((response) => response.json())
+    .then((data) => data['brl'])
+    .catch((err) => console.log('vish deu muito ruim'));
+
+  return final;
+}
 
 const fetchApi = async () => {
-  try {
-    const ul = document.getElementById('lista');
-    const response = await fetch(url);
-    const data = await response.json();
-    const keys = Object.keys(data);
-    data[keys[0]].filter(({ rank }) => rank <= 10 )
-      .map(({ symbol, name, priceUsd }) => {
-        const newLi = document.createElement('li');
-        newLi.innerText = `${name} (${symbol}): ${priceUsd}`
-        ul.appendChild(newLi);
-      });
-  } catch(err) {
-    console.log('deu ruim')
-  }
+  const url = 'https://api.coincap.io/v2/assets';
+
+  const final = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => data['data'])
+    .catch((err) => console.log('deu ruim dnv'));
+    
+  return final;
 };
 
-window.onload = fetchApi;
+const criar = async () => {
+  const valor = await fetchCripto();
+  const moeda = await fetchApi();
+
+  moeda.filter(({ rank }) => rank <= 10)
+    .forEach(({ symbol, name }) => {
+      const ul = document.getElementById('lista');
+      const newLi = document.createElement('li');
+
+      const value = valor[symbol.toLowerCase()];
+      newLi.innerText = `${name} (${symbol}): ${value}`;
+
+      ul.appendChild(newLi);
+    });
+};
+
+window.onload = criar;
